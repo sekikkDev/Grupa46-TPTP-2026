5df37e705e1352c936ffc25f3ed2307a74485fc8
sekikkDev <74156583+sekikkDev@users.noreply.github.com>
2026-05-08 04:55:04 +0200

    Loading animacija
    Loader koji se pojavljuje prije nego sto se kontent stranice ucita, dodan je na sve tri stranice.

---
+++ js/tptpskripte.js
@@ -0,0 +1,8 @@
+// ------------------ LOADER ------------------------
+
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.classList.add('hidden');
+
+    // kada se stranica loaduje skrivamo loader
+});
