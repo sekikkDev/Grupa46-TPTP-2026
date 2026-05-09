3c7ac19bf2adc009db0d53da1592b4060dcccd40
sekikkDev <74156583+sekikkDev@users.noreply.github.com>
2026-05-09 02:34:17 +0200

    JS Dark Mode
    Dodan sav kod namijenjen za glavni navigacijski meni, ukljucujuci i podrsku za mijenanje moda iz svijetlog u tamni, i obrnuto. Takodjer popravljena neka "nedefinisana" stanja prouzrokovana CSS-om

--- js/tptpskripte.js
+++ js/tptpskripte.js
@@ -1,3 +1,15 @@
+//dark mode toggle ostaje fokusiran nakon klika, da bi se toga rijesili dodajemo ovo
+
+document.querySelector('.dark-mode-label').addEventListener('click', () => {
+    document.getElementById('dark-mode-toggle').blur();
+});
+
+// isto tako i github logo
+
+document.querySelector('.github-btn').addEventListener('click', () => {
+    document.querySelector('.github-link').blur();
+});
+
 // ------------------ LOADER ------------------------
 
 window.addEventListener('load', () => {
@@ -5,4 +17,29 @@ window.addEventListener('load', () => {
     loader.classList.add('hidden');
 
     // kada se stranica loaduje skrivamo loader
+});
+
+
+// ------------------------- DARK MODE ------------------------------
+
+const toggle = document.getElementById('dark-mode-toggle');
+
+// pri ucitavanju stranice provjeravamo da li vec postoji neka tema u localStorage
+if (localStorage.getItem('tema') === 'dark') {
+        // provjeravamo samo dark temu jer je light tema default
+        document.body.classList.add('dark');
+        toggle.checked = true;
+
+        // dodajemo .dark klasu na body, cime se se promijeniti --current varijable u css i jos neki dijelovi definisani sa body.dark
+        // checkiramo dark mode element, da bi se aktivirao css ikonice u navigaciji
+}
+
+toggle.addEventListener('change', () => {
+    // kada kliknemo na dark mode btn
+    // na body element dodajemo klasu .dark (objasnjeno u proslom komentaru ↑)
+    document.body.classList.toggle('dark', toggle.checked);
+    
+    // sacuvamo odabir u local storage na osnovu toga da li je toggle checkiran ili ne
+    // DA = dark ; NE = light
+    localStorage.setItem('tema', toggle.checked ? 'dark' : 'light');
 });
